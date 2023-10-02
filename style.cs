ac5d724fb2881c90639529543a6db43bf88fa484
susuh777 <141043560+susuh777@users.noreply.github.com>
2023-10-02 12:01:34 -0300

    Update style.cs

--- style.cs
+++ style.cs
@@ -4,4 +4,12 @@ header {
 
 nav a {
   text-transform: uppercase;
+  color: #000000;
+  font-weight: bold;
+  font-size: 22px;
+  text-decoration: none;
+}
+nav Li {
+  display: inline;
+  margin:
 }
